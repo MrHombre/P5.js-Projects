1acc52a4550ab99ca0e52a7324620af88b7b95eb
James Lett <jlett89@gmail.com>
2016-09-21 20:08:26 -0700

    create seed is working
    now need source code

---
+++ p1_diastic/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  noCanvas();
+
+  var seed = select("#seed");
+  var submit = select("#submit");
+  submit.mousePressed(function() {
+    createP(seed.value());
+  });
+
+
+
+
+
+}
