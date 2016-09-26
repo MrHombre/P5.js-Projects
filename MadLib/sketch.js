03ca7ae868f3a31ef57d854cccff595f0256aaab
James Lett <jlett89@gmail.com>
2016-09-26 13:14:44 -0700

    add mad lib text to index, created button

--- MadLib/sketch.js
+++ MadLib/sketch.js
@@ -2,12 +2,19 @@
 var sheet;
 
 function setup() {
-
+  noCanvas(); //Remove that Canvas
   Tabletop.init( {
     key: '1grB0v27HNKxTppSnBL4zQ67I00v_vfrL40hUNy5MnI8',
       callback: gotData,
       simpleSheet: true
-    } )
+    });
+
+    var button = createButton('generate madlib'); //Gen button
+    button.mousePressed(generate);
+}
+
+function generate() {
+  console.log('generate'); //Testing to make sure things work
 }
 
 function gotData(data, tabletop) {
